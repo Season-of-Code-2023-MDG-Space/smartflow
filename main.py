10fefd84e1e39d9fa707dff2fb809767d200ff28
vrag99 <garv.codes@gmail.com>
2023-03-03 23:22:56 +0530

    feat: dynamically changing the project's name and version while cloning from a remote template repo

--- main.py
+++ main.py
@@ -4,6 +4,24 @@ import json
 from github import Github
 import requests
 import sys
+import re
+
+def SubstituteNameAndVersion(string, bin=False):
+    '''
+    Substitutes the project name and project's version
+    in the string given.
+    ------
+    Parameters:
+    1. string: The content where the variables need to be substituted.
+    2. bin: If the provided data is in bytes. It's False by default.
+    '''
+    if bin:
+        string = string.replace(b"__project_name__", bytes(project_name, encoding='utf-8'))
+        string = string.replace(b"__project_version__", bytes(version, encoding='utf-8'))
+    else:
+        string = re.sub(r'__project_name__', project_name, string)
+        string = re.sub(r'__project_version__', version, string)
+    return string
 
 # Getting the access token
 if 'access_token.json' not in os.listdir("./src"):
@@ -12,46 +30,41 @@ if 'access_token.json' not in os.listdir("./src"):
 with open(os.path.normpath('./src/access_token.json'), 'r') as file:
     access_token = json.load(file)['access_token']
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     usage = '''
-    Usage : python3 main.py <project_name> <framework>
-    Currently supported frameworks -->
-    * React.js
+    Usage : python3 main.py <project_name> <project_version> <template_link>
     '''
     print(usage)
     
 else: 
     g = Github(access_token)
-    project_name, framework = sys.argv[1:]
-
-    if framework.lower().startswith('react'):
-        with open(os.path.normpath('./templateConfig.json'), 'r') as configFile:
-            repo_link = json.load(configFile)['react']
+    project_name, version, template_link = sys.argv[1:]
+    template = re.findall(r"https://github\.com/(.*)", template_link)[0]
     
-        repo = g.get_repo(repo_link)
-        contents = repo.get_contents("")
-
-        while contents:
-            
-            # Listing out all the structure of the template.
-            file_content = contents.pop(0)
-            if file_content.type == "dir":
-                contents.extend(repo.get_contents(file_content.path))
-                
-            else:
-                path = os.path.join(project_name, file_content.path)
-
-                if not os.path.exists(os.path.dirname(path)): # Making the required folders
-                    os.makedirs(os.path.dirname(path))
-                    
-                res = requests.get(file_content.download_url) # Saving the files
-                with open(path, 'wb') as f:
-                    f.write(res.content)
-                
-                print(f"\u2705 {path}")
-                
-        print("\nGood to Go!\n")
-    else: 
-        print("Support coming soon... \N{smiling face with halo}")
+    repo = g.get_repo(template)
+    contents = repo.get_contents("")
+          
+    while contents:
         
+        # Listing out all the structure of the template.
+        file_content = contents.pop(0)
+        if file_content.type == "dir":
+            contents.extend(repo.get_contents(file_content.path))
+        
+        else:
+            path = os.path.join(project_name, file_content.path)
+            path = SubstituteNameAndVersion(path) # Substituting name and version in file names.
 
+            # Making required folders
+            if not os.path.exists(os.path.dirname(path)): 
+                os.makedirs(os.path.dirname(path))
+            
+            res = requests.get(file_content.download_url)
+            with open(path, 'wb+') as f:
+                # Writing the substituted content onto the file
+                f.write(SubstituteNameAndVersion(res.content, bin=True)) 
+                
+            print(f"\u2705 {path}")
+            
+    print("\nGood to Go!\n")
+            
